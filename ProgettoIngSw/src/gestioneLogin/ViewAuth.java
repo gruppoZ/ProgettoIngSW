7c550bca5da53489a478558509e89b5eba6a06c7
youss <youss@192.168.56.1>
2022-04-03 16:12:38 +0200

    Correzione bug:
    Registrandosi con successo (fruitore) non si viene portati alla chermata
    di login, ma alla schermata principale

--- ProgettoIngSw/src/gestioneLogin/ViewAuth.java
+++ ProgettoIngSw/src/gestioneLogin/ViewAuth.java
@@ -5,18 +5,20 @@ import it.unibs.fp.mylib.InputDati;
 
 public abstract class ViewAuth {
 
-	protected static final String INTESTAZIONE_LOGIN = "Login %s";
+	protected static final String INTESTAZIONE_LOGIN = "\nLogin %s";
 	protected static final String INTESTAZIONE_REGISTRAZIONE = "Registrazione %s";
 	private static final String MSG_RIPETI_LOGIN = "Vuoi rifare il login?";
 	protected static final String MSG_ERRORE_CREDENZIALI_DEFAULT = "ATTENZIONE: CREDENZIALI ERRATE";
 	protected static final String MSG_USERNAME_ESISTENTE ="ATTENZIONE: USERNAME GIA' ESISTENTE";
 	protected static final String MSG_PASSWORD_ERRATA ="ATTENZIONE: PASSWORD ERRATA";
-	protected static final String MSG_LOGIN_EFFETTUATO = "Login avvenuto con successo";
+	protected static final String MSG_LOGIN_EFFETTUATO = "\nLogin avvenuto con successo!\n";
 	private static final String MSG_USERNAME ="Inserisci lo username desiderato:";
 	private static final String MSG_PASSWORD ="Inserisci la password desiderata:";
 	protected static final String MSG_USERNAME_LOGIN ="Inserisci lo username:";
 	protected static final String MSG_PASSWORD_LOGIN ="Inserisci la password:";
 	protected static final String MSG_CREDENZIALI_ERRATE = "\nCredenziali errate!";
+	protected static final String MSG_RESISTRAZIONE_SUCCESSO_GIVE_CREDENZIALI = "Registrazione avvenuta con successo!\nLo username e': %s la password e': %s \n";
+	
 	
 	private GestioneAutenticazione gestoreAuth;
 	
@@ -57,9 +59,8 @@ public abstract class ViewAuth {
 		
 		credenziali = new Credenziali(username, password);
 		
-		System.out.println("Registrazione avvenuta con successo");
-		System.out.println("Lo username e': " + username + " la password e': " + password);
-
+		System.out.printf(MSG_RESISTRAZIONE_SUCCESSO_GIVE_CREDENZIALI, username, password);
+		
 		this.gestoreAuth.effettuaRegistrazione(credenziali, utente);
 	}
 	
